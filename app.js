3d8489a710312cccba44d9282f4085790fae274f
Darren Klein <darren.a.klein@gmail.com>
2018-08-26 17:23:30 -0400

    Read from the owner_view table, able to render data as a table.

--- app.js
+++ app.js
@@ -1,10 +1,56 @@
+var ownerTable = $('table')[0];
 
-var fetchOwnerData = function() {
-	$.post('./get_all_owners.php', {}, function(data) {
-		console.log(JSON.parse(data))
+// We can assume that all of the rows of data will have the same keys -
+// so grab a sample row and pull the keys off to be used in the table header and as a reference for the other rows.
+var makeRowMap = function(sampleRow) {
+	return Object.keys(sampleRow);
+};
+
+var makeHeaderRow = function(rowMap) {
+	var headerRow = document.createElement('tr');
+
+	for (var i = 0; i < rowMap.length; i++) {
+		var tableHeader = document.createElement('th');
+		tableHeader.innerHTML = rowMap[i];
+		headerRow.append(tableHeader);
+	};
+
+	return ownerTable.append(headerRow);
+};
+
+var makeTableRow = function(rowMap, ownerData) {
+	for (var i = 0; i < ownerData.length; i++) {
+		var tableRow = document.createElement('tr');
+
+		for (var j = 0; j < rowMap.length; j++) {
+			var tableCell = document.createElement('td');
+			tableCell.innerHTML = ownerData[i][rowMap[j]];
+			tableRow.append(tableCell);
+		};
+
+		ownerTable.append(tableRow);
+	};
+
+	return undefined;
+};
+
+var makeRows = function(ownerData) {
+	var rowMap = makeRowMap(ownerData[0]);
+	makeHeaderRow(rowMap, ownerData);
+	makeTableRow(rowMap, ownerData);
+	return undefined;
+};
+
+var fetchOwnerData = function(callback) {
+	$.post('./get_all_owners.php', {}, function(ownerData) {
+		if (ownerData.length === 0) {
+			return [];
+		}
+
+		return callback(JSON.parse(ownerData));
 	});
 };
 
 $(document).ready(function() {
-	return fetchOwnerData()
+	return fetchOwnerData(makeRows);
 });
--- get_all_owners.php
+++ get_all_owners.php
@@ -2,11 +2,10 @@
 include "credentials.php";
 
 $conn = pg_connect("host=$host dbname=$dbname user=$user password=$password");
-$result = pg_query($conn, "SELECT * FROM owner");
+$result = pg_query($conn, "SELECT * FROM owner_view");
 
 # pg_fetch_all will retun the results as a list of objects, which can then be JSON-encoded and passed to the client.
 print(json_encode(pg_fetch_all($result)));
 
 pg_close();
-
 ?>
