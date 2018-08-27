6db1ecc6a569408b8a8d5a1f64d50f96beaa192f
Darren Klein <darren.a.klein@gmail.com>
2018-08-26 20:08:12 -0400

    Refactor PHP, att toLowerCase() to the sorting method so last names starting with lowercase are handled appropriately.

--- app.js
+++ app.js
@@ -2,31 +2,31 @@ var ownerTable = $('table')[0];
 
 // We can assume that all of the rows of data will have the same keys -
 // so grab a sample row and pull the keys off to be used in the table header and as a reference for the other rows.
-var makeRowMap = function(sampleRow) {
+var makeRowKeys = function(sampleRow) {
 	return Object.keys(sampleRow);
 };
 
 // Populate either kind of row - either header or just 'normal' data.
-var populateRow = function(cellType, rowMap, owner) {
+var populateRow = function(cellType, rowKeys, owner) {
 	var tableRow = document.createElement('tr');
 
-	for (var i = 0; i < rowMap.length; i++) {
+	for (var i = 0; i < rowKeys.length; i++) {
 		var tableCell = document.createElement(cellType);
-		tableCell.innerHTML = owner ? owner[rowMap[i]] : rowMap[i];
+		tableCell.innerHTML = owner ? owner[rowKeys[i]] : rowKeys[i];
 		tableRow.append(tableCell);
 	};
 
 	return tableRow;
 };
 
-var appendHeaderRow = function(rowMap) {
-	var headerRow = populateRow('th', rowMap);
+var appendHeaderRow = function(rowKeys) {
+	var headerRow = populateRow('th', rowKeys);
 	ownerTable.append(headerRow);
 };
 
-var appendTableRows = function(rowMap, ownerData) {
+var appendTableRows = function(rowKeys, ownerData) {
 	for (var i = 0; i < ownerData.length; i++) {
-		var dataRow = populateRow('td', rowMap, ownerData[i])
+		var dataRow = populateRow('td', rowKeys, ownerData[i])
 		ownerTable.append(dataRow);
 	};
 };
@@ -34,15 +34,28 @@ var appendTableRows = function(rowMap, ownerData) {
 // Populate the rows and append them to the table in the DOM.
 // Passed as a callback to fetchOwnerData().
 var makeRows = function(ownerData) {
-	var rowMap = makeRowMap(ownerData[0]);
-	appendHeaderRow(rowMap);
-	appendTableRows(rowMap, ownerData);
+	var rowKeys = makeRowKeys(ownerData[0]);
+	appendHeaderRow(rowKeys);
+	appendTableRows(rowKeys, ownerData);
+};
+
+// Sort the owners by last name.
+// Note that, for the purposes of evaluating alphabetical position, last names will be lowercased.
+var sortByLastName = function(a, b) {
+	if (a.last_name.toLowerCase() < b.last_name.toLowerCase()) {
+		return -1;
+	} else if (a.last_name > b.last_name) {
+		return 1;
+	}
+
+	return 0;
 };
 
 var fetchOwnerData = function(callback) {
 	$.post('./get_all_owners.php', {}, function(ownerData) {
 		if (ownerData.length > 0) {
-			return callback(JSON.parse(ownerData));
+			var sortedOwnerData = JSON.parse(ownerData).sort(sortByLastName);
+			return callback(sortedOwnerData);
 		}
 	});
 };
--- get_all_owners.php
+++ get_all_owners.php
@@ -1,11 +1,32 @@
 <?php
 include "credentials.php";
 
-$conn = pg_connect("host=$host dbname=$dbname user=$user password=$password");
-$result = pg_query($conn, "SELECT * FROM owner_view");
+function connect($host, $dbname, $user, $password) {
+	$conn = pg_connect("host=$host dbname=$dbname user=$user password=$password");
+
+	if ($conn) {
+		return($conn);
+	} else {
+		// Not sure to what extent this is useful, right now...
+        trigger_error("Database connection error.");
+	}
+};
+
+function get_owner_data($conn) {
+	$results = pg_query($conn, "SELECT * FROM owner_view");
+	return($results);
+};
 
 # pg_fetch_all will retun the results as a list of objects, which can then be JSON-encoded and passed to the client.
-print(json_encode(pg_fetch_all($result)));
+function convert_to_list($raw_data) {
+	return(pg_fetch_all($raw_data));
+};
+
+$conn = connect($host, $dbname, $user, $password);
+$raw_data = get_owner_data($conn);
+$data = convert_to_list($raw_data);
+
+print(json_encode($data));
 
 pg_close();
 ?>
